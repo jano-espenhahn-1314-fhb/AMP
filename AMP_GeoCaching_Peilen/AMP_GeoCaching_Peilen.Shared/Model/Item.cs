e4afa4159b47039f8701d770834e4244d46bc9ba
jano-espenhahn-1314-fhb <jano.espenhahn@posteo.de>
2015-06-02 09:22:27 +0200

    ausgeschlossene Klassen und Bibliotheken

---
+++ AMP_GeoCaching_Peilen/AMP_GeoCaching_Peilen.Shared/Model/Item.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace AMP_GeoCaching_Peilen.Model
+{
+    class Item : INotifyPropertyChanged
+    {
+        public string Name { get; set; }
+        public object Value { get; set; }
+
+        // The number of each item that has been collected.
+        private int _count;
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+            set
+            {
+                _count = value;
+                RaisePropertyChanged("Count");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
